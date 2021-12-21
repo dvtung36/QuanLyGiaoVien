d27079535f1db2dea9501354cdd5b4531fe2adac
HoangLV <levanhoang0699@gmail.com>
2021-12-21 21:22:08 +0700

    okeee

---
+++ QuanLyGiaoVien/QuanLyGiaoVien/Controllers/ChucVuChinhQuyenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyGiaoVien.Controllers
+{
+    public class ChucVuChinhQuyenController : Controller
+    {
+        // GET: ChucVuChinhQuyen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuanLyGiaoVien/QuanLyGiaoVien/Controllers/LopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyGiaoVien.Controllers
+{
+    public class LopController : Controller
+    {
+        // GET: Lop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
